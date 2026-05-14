40ea90914becfcadcc7025d121486c386698ff9d
Skyerp <skyerp.skylabtech@gmail.com>
2026-05-14 12:14:55 +0530

    fix

--- application/config/config.php
+++ application/config/config.php
@@ -39,11 +39,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['index_page'] = '';
-$config['base_url'] = 'http://localhost:8888/aaauniversity/';
-// $config['base_url'] = 'https';
-// //$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-// $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
-// $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
+$config['base_url'] = 'https';
+//$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 /*
 
@@ -237,7 +237,7 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 4;
+$config['log_threshold'] = 0;
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/logs/log-2026-05-14.php
@@ -0,0 +1,85 @@
+INFO - 2026-05-14 06:43:24 --> Config Class Initialized
+INFO - 2026-05-14 06:43:24 --> Hooks Class Initialized
+DEBUG - 2026-05-14 06:43:24 --> UTF-8 Support Enabled
+INFO - 2026-05-14 06:43:24 --> Utf8 Class Initialized
+INFO - 2026-05-14 06:43:24 --> URI Class Initialized
+INFO - 2026-05-14 06:43:24 --> Router Class Initialized
+INFO - 2026-05-14 06:43:24 --> Output Class Initialized
+INFO - 2026-05-14 06:43:24 --> Security Class Initialized
+DEBUG - 2026-05-14 06:43:24 --> Global POST, GET and COOKIE data sanitized
+INFO - 2026-05-14 06:43:24 --> Input Class Initialized
+INFO - 2026-05-14 06:43:24 --> Language Class Initialized
+INFO - 2026-05-14 12:13:24 --> Loader Class Initialized
+INFO - 2026-05-14 12:13:24 --> Helper loaded: url_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: file_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: form_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: security_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: string_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: inflector_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: directory_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: download_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: user_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: multi_language_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: common_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: pagination_helper
+INFO - 2026-05-14 12:13:24 --> Helper loaded: addon_helper
+INFO - 2026-05-14 12:13:24 --> XML-RPC Class Initialized
+INFO - 2026-05-14 12:13:24 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2026-05-14 12:13:24 --> Pagination Class Initialized
+INFO - 2026-05-14 12:13:24 --> Model "User_model" initialized
+INFO - 2026-05-14 12:13:24 --> Model "Crud_model" initialized
+INFO - 2026-05-14 12:13:24 --> Model "Video_model" initialized
+INFO - 2026-05-14 12:13:24 --> Model "Email_model" initialized
+INFO - 2026-05-14 12:13:24 --> Model "Payment_model" initialized
+INFO - 2026-05-14 12:13:24 --> Model "Lazyloaddata_model" initialized
+INFO - 2026-05-14 12:13:24 --> Model "Api_model" initialized
+INFO - 2026-05-14 12:13:24 --> Model "Addon_model" initialized
+INFO - 2026-05-14 12:13:24 --> Controller Class Initialized
+INFO - 2026-05-14 12:13:24 --> Database Driver Class Initialized
+INFO - 2026-05-14 12:13:28 --> Session: Class initialized using 'database' driver.
+INFO - 2026-05-14 12:13:28 --> Form Validation Class Initialized
+INFO - 2026-05-14 12:13:28 --> File loaded: C:\wamp64\www\aaauniversity\application\views\backend/index.php
+INFO - 2026-05-14 12:13:28 --> Final output sent to browser
+DEBUG - 2026-05-14 12:13:28 --> Total execution time: 4.2161
+INFO - 2026-05-14 06:43:29 --> Config Class Initialized
+INFO - 2026-05-14 06:43:29 --> Hooks Class Initialized
+DEBUG - 2026-05-14 06:43:29 --> UTF-8 Support Enabled
+INFO - 2026-05-14 06:43:29 --> Utf8 Class Initialized
+INFO - 2026-05-14 06:43:29 --> URI Class Initialized
+INFO - 2026-05-14 06:43:29 --> Router Class Initialized
+INFO - 2026-05-14 06:43:29 --> Output Class Initialized
+INFO - 2026-05-14 06:43:29 --> Security Class Initialized
+DEBUG - 2026-05-14 06:43:29 --> Global POST, GET and COOKIE data sanitized
+INFO - 2026-05-14 06:43:29 --> Input Class Initialized
+INFO - 2026-05-14 06:43:29 --> Language Class Initialized
+INFO - 2026-05-14 12:13:29 --> Loader Class Initialized
+INFO - 2026-05-14 12:13:29 --> Helper loaded: url_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: file_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: form_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: security_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: string_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: inflector_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: directory_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: download_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: user_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: multi_language_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: common_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: pagination_helper
+INFO - 2026-05-14 12:13:29 --> Helper loaded: addon_helper
+INFO - 2026-05-14 12:13:29 --> XML-RPC Class Initialized
+INFO - 2026-05-14 12:13:29 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2026-05-14 12:13:29 --> Pagination Class Initialized
+INFO - 2026-05-14 12:13:29 --> Model "User_model" initialized
+INFO - 2026-05-14 12:13:29 --> Model "Crud_model" initialized
+INFO - 2026-05-14 12:13:29 --> Model "Video_model" initialized
+INFO - 2026-05-14 12:13:29 --> Model "Email_model" initialized
+INFO - 2026-05-14 12:13:29 --> Model "Payment_model" initialized
+INFO - 2026-05-14 12:13:29 --> Model "Lazyloaddata_model" initialized
+INFO - 2026-05-14 12:13:29 --> Model "Api_model" initialized
+INFO - 2026-05-14 12:13:29 --> Model "Addon_model" initialized
+INFO - 2026-05-14 12:13:29 --> Controller Class Initialized
+INFO - 2026-05-14 12:13:29 --> Database Driver Class Initialized
+INFO - 2026-05-14 12:13:31 --> Session: Class initialized using 'database' driver.
+INFO - 2026-05-14 12:13:31 --> Form Validation Class Initialized
+INFO - 2026-05-14 12:13:31 --> Final output sent to browser
+DEBUG - 2026-05-14 12:13:31 --> Total execution time: 1.7372
