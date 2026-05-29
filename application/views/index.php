8b6a577d7e99000eee24007ac8e9657257a72e9b
unknown <shruti@hucpl.com>
2026-05-29 16:34:27 +0530

    worked in gallery section

--- application/views/index.php
+++ application/views/index.php
@@ -790,16 +790,7 @@ h3{
             <div class="col-lg-3 col-sm-6">
                 <div class="edu-counterup counterup-style-3">
                     <h2 class="counter-item count-number primary-color">
-                        <?php $total_user = $this->crud_model->get_only_user()->num_rows();
-                        $change_value = $total_user * 11;
-
-                        $data = $this->lazyload->thousand_upper_number_change_in_k($change_value);
-                        $new_data = explode("/", $data)
-                        //                                echo $data;
-                        //                                die();
-                        ?>
-                        <span class="odometer"
-                              data-odometer-final="<?php echo $new_data[0]; ?>">.</span><span><?php echo $new_data[1]; ?></span>
+                        <span class="odometer" data-odometer-final="100">.</span><span>+</span>
                     </h2>
                     <h6 class="title">Student Enrolled</h6>
                 </div>
@@ -807,7 +798,7 @@ h3{
             <div class="col-lg-3 col-sm-6">
                 <div class="edu-counterup counterup-style-3">
                     <h2 class="counter-item count-number secondary-color">
-                        <span class="odometer" data-odometer-final="32.4">.</span><span>K</span>
+                        <span class="odometer" data-odometer-final="2">.</span><span>+</span>
                     </h2>
                     <h6 class="title">Class Completed</h6>
                 </div>
