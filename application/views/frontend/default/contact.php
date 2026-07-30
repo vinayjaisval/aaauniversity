554936cd53c5130e4ba7fdf4d099179103421a37
Skyerp <skyerp.skylabtech@gmail.com>
2026-07-30 13:05:31 +0530

    location change

--- application/views/frontend/default/contact.php
+++ application/views/frontend/default/contact.php
@@ -122,11 +122,8 @@
 <div class="google-map-area">
     <div class="mapouter">
         <div class="gmap_canvas">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3562.997687143012!2d83.38055779999999!3d26.7444499!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39914501691b608d%3A0xe4289ebd31708cf8!2sEkon%20Academy!5e0!3m2!1sen!2sin!4v1668763142039!5m2!1sen!2sin"
-                    width="100%" height="600px" allowfullscreen="" frameborder="0" scrolling="no" marginheight="0"
-                    marginwidth="0" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            <!--                    <iframe id="gmap_canvas" src="https://goo.gl/maps/dnTU4adVbWWoUuBg9" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>-->
-        </div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d28023.002678156183!2d77.29743734135744!3d28.603516505325675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x390d1e22a75832bb%3A0xae72762e73c41040!2sOverseas%20Education%20Lane%2C%201st%20floor%2C%20B%2037%2C%20B%20Block%2C%20Sector%202%2C%20Noida%2C%20Uttar%20Pradesh%20201301!3m2!1d28.5871727!2d77.31666059999999!5e0!3m2!1sen!2sin!4v1785396893461!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+            </div>
     </div>
 </div>
 
